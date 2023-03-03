ff48827c2d5bbbce7458e5362ca5d4599e6f08f0
Elise Rosky <emrosky@mtu.edu>
2023-03-03 14:31:43 -0700

    cloudpass searching

--- Tools/escape_nc_search.m
+++ Tools/escape_nc_search.m
@@ -8,14 +8,18 @@ function [indices, timestamps] = escape_nc_search(ncfile)
     % longer.
 
     % Define LWC threshold for cloud
-    LWC_threshold = 0.7; % g/m3
-    duration_threshold = 10; % seconds
+    LWC_threshold = 0.5; % g/m3
+    duration_threshold = 3; % seconds
 
     % Get data from the netCDF file
     time = ncread(ncfile,'Time');
     cdplwc = ncread(ncfile,'lwc_cdp_sp_rt'); % cdp liquid water content
     nevzlwc = ncread(ncfile, 'lwc_nevz_sp_rt'); % nevzerov liquid water content
     nevztwc = ncread(ncfile, 'twc_nevz_sp_rt'); % nevzerov total water content
+    
+    % Reformat time to human readable format
+    % Given in netcdf file as seconds since 1970-01-01 +0000
+    time2 = datetime(1970,1,1) + seconds(time(:,1));
     
     % Which lwc value to use for the search
     LWC = cdplwc;
@@ -41,16 +45,19 @@ function [indices, timestamps] = escape_nc_search(ncfile)
       end
     end
     % Display the regions that meet the criteria:
-    celldisp(out)
+    celldisp(out);
     
     for p = 1 : length(indices)
         i_start = indices{p}(1);
         i_end = indices{p}(end);
-        s_start = datetime(i_start, 'convertfrom', 'posixtime', 'Format', 'MM/dd/yy HH:mm:ss.SSS');
-        s_end = datetime(i_end, 'convertfrom', 'posixtime', 'Format', 'MM/dd/yy HH:mm:ss.SSS');
-        s_start.Format = 'hh:mm:ss.SSS';
-        s_end.Format = 'hh:mm:ss.SSS';
-        timestamps{p} = [s_start, s_end];
+        start_time = time(i_start);
+        end_time = time(i_end);
+        s_start = datetime(start_time, 'convertfrom', 'posixtime', 'Format', 'MM/dd/yy HH:mm:ss.SSS');
+        s_end = datetime(end_time, 'convertfrom', 'posixtime', 'Format', 'MM/dd/yy HH:mm:ss.SSS');
+        s_start.Format = 'HH:mm:ss.SSS';
+        s_end.Format = 'HH:mm:ss.SSS';
+        %timestamps{p} = [s_start, s_end];
+        timestamps{p} = [datenum(time2(i_start)), datenum(time2(i_end))];
     end
     
 
